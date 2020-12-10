43e8c27afa999463fd773e16ef97d0f6b49c4c20
xitedemon <xitedemon@gmail.com>
2020-12-10 14:29:19 +0530

    Initial commit

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -1,43 +1,15 @@
-
-
 module.exports = {
     options: {
         map: false,
         processors: [
-            require('pixrem')(),
-            require('autoprefixer')({cascade: true})
-/*
             require('autoprefixer')({
-                //browsers: ['last 4 versions'],
                 cascade: true,
-                add: true
-                //grid: "autoplace" // use false is preffered
+                add: true,
+                //grid: "autoplace"
             })
-            */
-
         ]
     },
     demo: {
-        src: ['temp/css/**/*.css', '!temp/css/vendor/**', '!temp/css/**/*.min.css']
+        src: ['temp/css/**/*.css', '!temp/css/**/*.min.css', '!temp/css/theme-raw.css']
     }
 };
-
-
-/*
-    
-  "browserslist": [
-    "> 1%",
-    "ie >= 8",
-    "edge >= 15",
-    "ie_mob >= 10",
-    "ff >= 45",
-    "chrome >= 45",
-    "safari >= 7",
-    "opera >= 23",
-    "ios >= 7",
-    "android >= 4",
-    "bb >= 10"
-  ],
-    
-*/
-    
